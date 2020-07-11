3cce7d7e4cabc833ddcb8651f8f1c00d585b6bca
Samuel Reinhardt <sam@bytes.co>
2020-07-11 15:41:25 -0400

    Initialize module

---
+++ readme.md
@@ -0,0 +1,7 @@
+# VectorArrow CacheButton
+
+## Short Description
+Adds a convenient cache clear button to the Magento admin
+
+## Contributors
+* Samuel William Reinhardt
---
+++ registration.php
@@ -0,0 +1,6 @@
+< php
+\Magento\Framework\Component\ComponentRegistrar::register(
+	\Magento\Framework\Component\ComponentRegistrar::MODULE,
+	'VectorArrow_CacheButton',
+	__DIR__
+);
