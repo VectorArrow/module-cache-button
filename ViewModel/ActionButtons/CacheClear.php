0c3a05f1afa4ca4d1f206e1adc36e562a54e3a7f
Samuel Reinhardt <sam@bytes.co>
2020-08-09 19:38:50 -0400

    Update dependency and set version to 1.0.0
    Update changelog

--- ViewModel/ActionButtons/CacheClear.php
+++ ViewModel/ActionButtons/CacheClear.php
@@ -2,9 +2,10 @@
 
 class CacheClear implements \Magento\Framework\View\Element\Block\ArgumentInterface {
     
-    /** \Magento\Backend\Model\Url **/
+    /** @var \Magento\Backend\Model\Url **/
     protected $backend_url_resolver;
 
+    /** @var string **/
     protected $cache_flush_path = 'adminhtml/cache/flushSystem';
 
     public function __construct(
@@ -13,10 +14,18 @@ class CacheClear implements \Magento\Framework\View\Element\Block\ArgumentInterf
         $this->backend_url_resolver = $backend_url_resolver;
     }
 
+    /**
+     * Get a url for the Magento backend
+     * @return string
+     */
     public function getUrl( $route_path = null, $route_params = null ) {
         return $this->backend_url_resolver->getUrl( $route_path, $route_params );
     }
 
+    /**
+     * Get the Magento cache flush url
+     * @return string
+     */
     public function getCacheFlushUrl() {
         return $this->getUrl( $this->cache_flush_path );
     }
--- changelog.md
+++ changelog.md
@@ -1,4 +1,7 @@
 # Changelog
+## [1.0.0]
+- Cache clear functionality added.
+
 ## [0.0.1]
 ### Added
 - Module intialized
